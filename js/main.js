a2338c8d9c31d0a05a26bd74944bfb77b8ebc8e7
hyper_X_octane <shrinaveen6053@gmail.com>
2020-09-22 12:31:10 +0530

    site color and animation is changed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
+let count=0;
 $(document).ready(function loop() {
-  $('.circle1-pos').animate({top:"-95"},5000,"swing");
-  $('.circle1-pos').delay(1000).animate({top:"-135"},6000,"swing",loop);
+  if (count === 0) {
+    $('.circle1-pos').animate({top: "-95"}, 3000, "swing");
+    $('.circle1-pos').animate({top: "-155"}, 3000, "swing");
 
-  $('.circle2-pos').animate({top:"100"},5000,"swing");
-  $('.circle2-pos').animate({top:"155"},5000,"swing",loop);
+    $('.circle2-pos').animate({top: "90"}, 3000, "swing");
+    $('.circle2-pos').animate({top: "165"}, 3000, "swing");
 
-  $('.circle3-pos').delay(2000).animate({top:"450"},5000,"swing");
-  $('.circle3-pos').delay(2000).animate({top:"555"},5000,"swing",loop);
+    $('.circle3-pos').animate({top: "450"}, 3000, "swing");
+    $('.circle3-pos').animate({top: "555"}, 3000, "swing");
 
-  $('.circle5-pos').animate({top:"1400"},5000,"swing");
-  $('.circle5-pos').animate({top:"1450"},5000,"swing",loop);
+    $('.circle5-pos').animate({top: "1400"}, 3000, "swing");
+    $('.circle5-pos').animate({top: "1450"}, 3000, "swing");
+  }
+
 });
+$('.circle4-pos').click(function (){
+  count =1;
+  $('.circle1-pos').animate({top: "-395"}, 700, "swing");
+  $('.circle2-pos').animate({top: "-200"}, 1200, "swing");
+  $('.circle3-pos').animate({top: "-650"}, 1500, "swing");
+  $('.circle4-pos').css("background","white");
+  $(document).css("background","black");
+  $('.find').animate({top:"800"},2000,"swing");
+});
+
 
